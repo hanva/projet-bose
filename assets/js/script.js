af7a47fc235444b703de79d60e0461f5d7c9701a
Nicolas <nicolas.tran@supinternet.fr>
2018-02-16 00:09:25 +0100

    rip

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,23 @@ $(document).ready(function () {
         preloadImages: "all",
         responsive: true
     });
-});
-$(function () {
-    var forEach = function (t, o, r) {
-        if ("[object Object]" === Object.prototype.toString.call(t))
-            for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
-        else
-            for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
-    };
+    $(function () {
+        var forEach = function (t, o, r) {
+            if ("[object Object]" === Object.prototype.toString.call(t))
+                for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+            else
+                for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+        };
 
-    var hamburgers = document.querySelectorAll(".hamburger");
-    if (hamburgers.length > 0) {
-        forEach(hamburgers, function (hamburger) {
-            hamburger.addEventListener("click", function () {
-                this.classList.toggle("is-active");
-            }, false);
-        });
-    }
-    $(document).ready(function () {
+        var hamburgers = document.querySelectorAll(".hamburger");
+        if (hamburgers.length > 0) {
+            forEach(hamburgers, function (hamburger) {
+                hamburger.addEventListener("click", function () {
+                    this.classList.toggle("is-active");
+                }, false);
+            });
+        }
+
         $('.nav').on('click', function () { // Au clic sur un élément
             var page = $(this).attr('href'); // Page cible
             var speed = 750; // Durée de l'animation (en ms)
@@ -35,8 +34,7 @@ $(function () {
     (function () {
         var hamburger = {
             navToggle: document.querySelector('.nav-toggle'),
-            nav: document.querySelector('nav'),
-
+            nav: document.querySelector('.navo'),
             open: function (e) {
                 e.preventDefault();
                 this.navToggle.classList.add('expanded');
@@ -48,7 +46,6 @@ $(function () {
                 this.nav.classList.remove('expanded');
             }
         };
-
         hamburger.navToggle.addEventListener('click', function (e) {
             if (hamburger.nav.classList.contains('expanded')) {
                 hamburger.close(e);
@@ -56,44 +53,14 @@ $(function () {
                 hamburger.open(e);
             }
         });
-
         hamburger.nav.addEventListener('click', function (e) {
             hamburger.open(e);
         });
-
-
-        document.addEventListener('click', function (e) {
+        mobile = document.querySelector("#mobile")
+        mobile.addEventListener('click', function (e) {
             if (e.path.indexOf(hamburger.navToggle) < 0 && e.path.indexOf(hamburger.nav) < 0) {
                 hamburger.close(e);
             }
         });
-
     }());
-    $(".hovered").mouseover(function () {
-        $(".hoveredd").slideDown(300);
-    });
 });
-$(".firstimage").mouseover(function () {
-    $(".hoveredd").slideUp(300);
-});
-$("body").click(function () {
-    $(".hoveredd").slideUp(300);
-});
-$(".buttonswag").click(function () {
-    $(".hoveredd").slideUp(300);
-});
-$("img").mouseover(function () {
-    $(".hoveredd").slideUp(300);
-});
-$("link").mouseover(function () {
-    $(".hoveredd").slideUp(300);
-});
-$("footer").mouseover(function () {
-    $(".hoveredd").slideUp(300);
-});
-
-
-
-function bigImg() {
-    alert("ok")
-}
